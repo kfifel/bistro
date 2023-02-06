75ce0898e34fae6fcd3972441437c1999831c567
khalid fifel <fifle.khalid99@gmail.com>
2023-02-06 14:03:40 +0100

    adding login and register pages

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'login')
+
+@section('content')
+    <h1>login</h1>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title', 'register')
+
+@section('content')
+    <h1>register</h1>
+@endsection
