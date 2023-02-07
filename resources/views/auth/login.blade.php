98c5d1ea98da61c562df3a5c9d6b279376415fba
khalid fifel <fifle.khalid99@gmail.com>
2023-02-07 11:29:22 +0100

    add login and register views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,5 +3,55 @@
 @section('title', 'login')
 
 @section('content')
-    <h1>login</h1>
+
+    <div class="w-40">
+        <h1>Login</h1>
+        <form method="POST" action="{{ route('login') }}" class="flex flex-col">
+            @csrf
+
+            <label for="email">Email</label>
+            <input
+                class="
+                    @error('email')
+                        error-border
+                     @enderror"
+                type="text"
+                name="email"
+                value="{{ old('email') }}"
+                id="email"
+            >
+
+            @error('email')
+            <div class="error">
+                {{ $message }}
+            </div>
+            @enderror
+
+            <label for="password">Password</label>
+            <input
+                class="
+                    @error('password')
+                        error-border
+                    @enderror"
+                type="password"
+                name="password"
+                id="password"
+            >
+
+            @error('password')
+            <div class="error">
+                {{ $message }}
+            </div>
+            @enderror
+
+            <label for="remember">
+                remember Me
+                <input type="checkbox" id="remember" name="remember">
+            </label>
+
+            <button type="submit" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">Login</button>
+
+        </form>
+    </div>
+
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,5 +3,54 @@
 @section('title', 'register')
 
 @section('content')
-    <h1>register</h1>
+    <section id="login" class=" w-full  bg-black text-red-500">
+
+        <!-- wrapper -->
+        <div class=" p-5 lg:px-20 flex flex-col md:flex-row items-center justify-center">
+
+            <!-- Contact Me -->
+            <form action="{{ route('register') }}" method="POST" class="w-full md:w-1/2 border border-red-500 p-6 bg-gray-900">
+
+                @csrf
+                <h2 class="text-2xl pb-3 font-semibold text-center">
+                    Login
+                </h2>
+                <div>
+                    <div class="flex flex-col mb-3">
+                        <label for="name">Name</label>
+                        <input
+                            type="text" id="name" name="name"
+                            class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                            autocomplete="off"
+                        >
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="email">Email</label>
+                        <input
+                            type="text" id="email" name="email"
+                            class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                            autocomplete="off"
+                        >
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                        >
+                    </div>
+                </div>
+                <div class="w-full pt-3">
+                    <button type="submit" class="w-full bg-gray-900 border border-red-500 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-red-500 hover:text-white text-xl cursor-pointer">
+                        Send
+                    </button>
+                </div>
+            </form>
+        </div>
+    </section>
 @endsection
