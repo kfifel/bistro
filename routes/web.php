c22395b7d6d949dee5c63a1cf8b97f076a07682b
khalid fifel <fifle.khalid99@gmail.com>
2023-02-07 11:36:33 +0100

    modify the route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,20 +12,21 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
-Route::resource('plats', \App\Http\Controllers\PlatController::class)
+Route::resource('plats', PlatController::class)
     ->except([
         'index', 'show'
     ])
     ->middleware(['auth']);
-Route::resource('plats', \App\Http\Controllers\PlatController::class)
+Route::resource('plats', PlatController::class)
     ->only([
         'show'
     ]);
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+Route::match(['GET', 'POST'], '/login',[AuthController::class, 'login'])
+    ->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+Route::match(['GET', 'POST'], '/register', [AuthController::class, 'register'])
+    ->name('register');
+
+Route::delete('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
