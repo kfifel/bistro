5b9ede0c0039b3079cb40f2dff87a2baa84d92c8
khalid fifel <fifle.khalid99@gmail.com>
2023-02-08 16:54:53 +0100

    add the alert notification to the layout and design the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,16 +3,19 @@
 @section('title', 'login')
 
 @section('content')
-    <section id="login" class="w-full bg-black text-red-500 ">
+    <section id="login" class="w-full bg-gray-500 text-gray-200 ">
 
         <!-- wrapper -->
         <div class=" p-20 lg:px-20 flex flex-col md:flex-row justify-center">
 
             <!-- Contact Me -->
-            <form action="{{ route('login') }}" method="POST" class="w-full md:w-1/2 border border-red-500 p-6 bg-gray-900">
+            <form action="{{ route('login') }}" method="POST" class="w-full md:w-1/2 border @if($errors->any()) border-red-500 @else border-black @endif p-6 bg-gray-700">
 
+                <div class="flex justify-center width-full">
+                    @includeWhen($errors->any(), '_error')
+                </div>
                 @csrf
-                <h2 class="text-2xl pb-3 font-semibold text-center">
+                <h2 class="text-3xl text-orange-500 pb-3 font-semibold text-center">
                     Login
                 </h2>
                 <div>
@@ -20,12 +23,18 @@
                         <label for="email">Email</label>
                         <input
                             type="text" id="email" name="email"
-                            class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                            class=" px-3 py-2 bg-gray-800 border
+                                    @error('email')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror
+                                    "
                             autocomplete="off"
                             value="{{ old('email') }}"
                         >
                         @error('email')
-                        <div class="error">
+                        <div class="text-red-600">
                             {{ $message }}
                         </div>
                         @enderror
@@ -37,10 +46,16 @@
                             id="password"
                             name="password"
                             value="{{ old('password') }}"
-                            class="px-3 py-2 bg-gray-800 border border-gray-900 focus:border-red-500 focus:outline-none focus:bg-gray-800 focus:text-red-500"
+                            class="px-3 py-2 bg-gray-800 border
+                            @error('email')
+                            border-red-500 bg-gray-800 text-red-500
+                            @else
+                            border-gray-900 outline-none
+                            @enderror
+                            "
                         >
                         @error('password')
-                        <div class="error">
+                        <div class="text-red-600">
                             {{ $message }}
                         </div>
                         @enderror
@@ -51,7 +66,7 @@
                     <input type="checkbox" id="remember" name="remember">
                 </label>
                 <div class="w-full pt-3">
-                    <button type="submit" class="w-full bg-gray-900 border border-red-500 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-red-500 hover:text-white text-xl cursor-pointer">
+                    <button type="submit" class="w-full bg-gray-900 border border-gray-200 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-green-600 hover:text-white text-xl cursor-pointer">
                         Connect
                     </button>
                 </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
     <title>@yield('title', 'mahlaba')</title>
 </head>
@@ -23,21 +24,24 @@
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                                 <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Home</a>
 
-                                <a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">about</a>
+                                <a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">About</a>
+
+                                <a href="{{ route('plats.index') }}" class="{{ request()->routeIs('plats.index') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">Plats</a>
                                 @auth
                                     <form action="{{ route('logout') }}" method="POST">
                                         @csrf
                                         @method("DELETE")
-                                        <button class="{{ request()->routeIs('logout') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">logout</button>
+                                        <button class="{{ request()->routeIs('logout') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">Logout</button>
                                     </form>
                                 @else
-                                <a href="{{ route('register') }}" class="{{ request()->routeIs('register') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">register</a>
+                                <a href="{{ route('register') }}" class="{{ request()->routeIs('register') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Register</a>
 
-                                <a href="{{ route('login') }}" class="{{ request()->routeIs('login') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">login</a>
+                                <a href="{{ route('login') }}" class="{{ request()->routeIs('login') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Login</a>
                                 @endauth
                             </div>
                         </div>
                     </div>
+                    @auth
                         <div class="ml-4 flex items-center md:ml-6">
                             <div class="relative ml-3">
                                 <div>
@@ -48,13 +52,42 @@
                                 </div>
                             </div>
                         </div>
+                    @endauth
                 </div>
             </div>
         </nav>
     </div>
 
     <main class="">
+        @if(session('success'))
+            <div id="toast-success" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Check icon</span>
+                </div>
+                <div class="ml-3 text-sm font-normal">{{ session('success') }}.</div>
+                <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+                    <span class="sr-only">Close</span>
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
+        @elseif(session('error'))
+            <div id="toast-danger" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Error icon</span>
+                </div>
+                <div class="ml-3 text-sm font-normal">{{ session('error') }}</div>
+                <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+                    <span class="sr-only">Close</span>
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+            </div>
+        @endif
+
         @yield('content')
     </main>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 </body>
 </html>
