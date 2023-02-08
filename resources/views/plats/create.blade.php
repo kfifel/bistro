166dfd9e0a697ed3febe06311cf0de51ac589e13
khalid fifel <fifle.khalid99@gmail.com>
2023-02-08 16:55:21 +0100

    add create plat

--- resources/views/plats/create.blade.php
+++ resources/views/plats/create.blade.php
@@ -1,1 +1,17 @@
-<?php
+@extends('layout')
+
+@section('title', 'create plat')
+
+@section('content')
+    <div class="">
+        <form action="{{ route('plats.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            <input type="text" name="title" class="b-black" value="{{ old('title') }}">
+            <input type="text" name="description" value="{{ old('description') }}">
+            <input type="file" name="image">
+
+            <button>save</button>
+        </form>
+    </div>
+@endsection
