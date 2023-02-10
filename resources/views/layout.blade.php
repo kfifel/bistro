919a1ec7e34c1b5134cae5d2785b5da339709bab
khalid fifel <fifle.khalid99@gmail.com>
2023-02-10 19:57:52 +0100

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,53 +14,55 @@
     <div id="header">
         <nav class="bg-gray-600">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="flex h-16 items-center justify-between">
-                    <div class="flex items-center">
+                <div class="h-16">
+                    <div class="flex items-center justify-between">
                         <div class="flex-shrink-0">
                             <img class="h-20 w-20" src="{{ url('images/logo.png') }}" alt="bistro">
                         </div>
                         <div class="hidden md:block">
-                            <div class="ml-10 flex items-baseline space-x-4">
+                            <div class="ml-10 flex items-baseline space-x-4 ">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                                <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'bg-orange-600 text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Home</a>
 
-                                <a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">About</a>
+                                <a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'bg-orange-600 text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">About</a>
 
-                                <a href="{{ route('plats.index') }}" class="{{ request()->routeIs('plats.index') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">Plats</a>
-                                @auth
-                                    <form action="{{ route('logout') }}" method="POST">
-                                        @csrf
-                                        @method("DELETE")
-                                        <button class="{{ request()->routeIs('logout') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">Logout</button>
-                                    </form>
-                                @else
-                                <a href="{{ route('register') }}" class="{{ request()->routeIs('register') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Register</a>
-
-                                <a href="{{ route('login') }}" class="{{ request()->routeIs('login') ? 'bg-gray-900 text-white hover:text-white' : 'text-gray-300 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Login</a>
-                                @endauth
+                                <a href="{{ route('plats.index') }}" class="{{ request()->routeIs('plats.index') ? 'bg-orange-600 text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }}  px-3 py-2 rounded-md text-sm font-medium">Plats</a>
                             </div>
                         </div>
-                    </div>
                     @auth
-                        <div class="ml-4 flex items-center md:ml-6">
-                            <div class="relative ml-3">
-                                <div>
-                                    <a href="#" class="flex max-w-xs items-center rounded-full bg-gray-500 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                        <span class="px-4">{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>
-                                        <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                    </a>
+                        <div class="container">
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button class="{{ request()->routeIs('logout') ? 'bg-orange-600 text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }} hover:text-white px-3 py-2 rounded-md text-sm font-medium">Logout</button>
+                            </form>
+                            <div class="ml-4 flex items-center md:ml-6">
+                                <div class="relative ml-3">
+                                    <div>
+                                        <a href="#" class="flex max-w-xs items-center rounded-full bg-gray-500 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                            <span class="px-4 text-white">{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>
+                                            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    @else
+                        <div>
+                            <a href="{{ route('register') }}" class="{{ request()->routeIs('register') ? 'bg-orange-600 text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Register</a>
+
+                            <a href="{{ route('login') }}" class="{{ request()->routeIs('login') ? 'bg-orange-600    text-white hover:text-white' : 'text-orange-600 hover:bg-gray-700' }} px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                        </div>
                     @endauth
+                    </div>
                 </div>
             </div>
         </nav>
     </div>
 
-    <main class="">
+    <main class=" p-6">
         @if(session('success'))
-            <div id="toast-success" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-success" class="flex items-center w-full p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Check icon</span>
@@ -72,7 +74,7 @@
                 </button>
             </div>
         @elseif(session('error'))
-            <div id="toast-danger" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="toast-danger" class="flex items-center w-full p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Error icon</span>
@@ -85,7 +87,10 @@
             </div>
         @endif
 
+        @yield('breadcrumbs')
+
         @yield('content')
+
     </main>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
--- resources/views/plats/edit.blade.php
+++ resources/views/plats/edit.blade.php
@@ -1,1 +1,20 @@
-<?php
+@extends('layout')
+
+@section('title', 'create plat')
+
+@section('content')
+    <div class="m-6 p-6">
+        <form action="{{ route('plats.update', $plat) }}" method="POST"
+              enctype="multipart/form-data" class="flex flex-col gap-6">
+            @csrf
+            @method('PUT')
+            <input type="text" name="title" class="b-black" value="{{ old('title', $plat->title) }}">
+            <input type="text" name="description" value="{{ old('description', $plat->description) }}">
+            <input type="file" name="image">
+
+            <button class="py-2 bg-blue-500 text-white text-bold text-xl hover:bg-blue-700 hover:rounded-xl">
+                save
+            </button>
+        </form>
+    </div>
+@endsection
