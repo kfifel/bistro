924286d520d43b4a1edae74dcff06da289429b9a
khalid fifel <fifle.khalid99@gmail.com>
2023-02-13 00:19:02 +0100

    add the profile sitting's to modify the password of name or email

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,9 +3,154 @@
 @section('title', 'Profile')
 
 @section('breadcrumbs')
-    @include('layouts.breadcrumbs', ['breadcrumbs' => $breadcrumbs])
+    @include('layouts.breadcrumbs', [ 'breadcrumbs' => ['Profile'] ]  )
 @endsection
 
 @section('content')
 
+<div class="flex flex-col sm:flex-row m-5">
+    <section id="login" class="w-full text-gray-200 ">
+
+        <!-- wrapper -->
+        <div class=" flex flex-col md:flex-row justify-center">
+
+            <!-- Contact Me -->
+            <form action="{{ route('profile.update') }}" method="POST" class="w-full md:w-3/4 border @if($errors->any()) border-red-500 @else border-black @endif p-6 bg-gray-700">
+
+                @csrf
+                @method('PUT')
+                <h2 class="text-2xl pb-3 font-semibold text-center">
+                    Update Profile
+                </h2>
+                <div>
+                    <div class="flex flex-col mb-3">
+                        <label for="name">Name</label>
+                        <input
+                            type="text" id="name" name="name"
+                            class="px-3 py-2 bg-gray-800 border
+                                    @error('name')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror"
+                            autocomplete="off"
+                            value="{{ old('name', Auth::user()->name) }}"
+                        >
+                        @error('name')
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="email">Email</label>
+                        <input
+                            type="text" id="email" name="email"
+                            class=" px-3 py-2 bg-gray-800 border
+                                    @error('email')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror
+                            "
+                            autocomplete="off"
+                            value="{{ old('email', Auth::user()->email) }}"
+                        >
+                        @error('email')
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="w-full pt-3">
+                    <button type="submit" class="w-full bg-gray-900 border border-gray-200 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-green-600 hover:text-white text-xl cursor-pointer">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </section>
+    <section id="login" class="w-full text-gray-200 ">
+
+        <!-- wrapper -->
+        <div class="flex flex-col md:flex-row justify-center">
+
+            <!-- Contact Me -->
+            <form action="{{ route('profile.update.password') }}" method="POST" class="w-full md:w-3/4 border @if($errors->any()) border-red-500 @else border-black @endif p-6 bg-gray-700">
+
+                @csrf
+                @method('PUT')
+                <h2 class="text-2xl pb-3 font-semibold text-center">
+                    Update password
+                </h2>
+                <div>
+                    <div class="flex flex-col mb-3">
+                        <label for="password">Old password</label>
+                        <input
+                            type="password" id="password" name="password"
+                            class="px-3 py-2 bg-gray-800 border
+                                    @error('password')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror"
+                            autocomplete="off"
+                        >
+                        @error('password')
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="newPassword">New password</label>
+                        <input
+                            type="password" id="newPassword" name="newPassword"
+                            class=" px-3 py-2 bg-gray-800 border
+                                    @error('newPassword')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror
+                            "
+                            autocomplete="off"
+                        >
+                        @error('newPassword')
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="flex flex-col mb-3">
+                        <label for="password_confirmation">Password confirmation</label>
+                        <input
+                            type="password"
+                            id="password_confirmation"
+                            name="password_confirmation"
+                            class="
+                                    px-3 py-2 bg-gray-800 border
+                                    @error('password_confirmation')
+                                    border-red-500 bg-gray-800 text-red-500
+                                    @else
+                                    border-gray-900 outline-none
+                                    @enderror
+                                    "
+                        >
+                        @error('password_confirmation')
+                        <div class="text-red-600">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="w-full pt-3">
+                    <button type="submit" class="w-full bg-gray-900 border border-gray-200 px-4 py-2 transition duration-50 focus:outline-none font-semibold hover:bg-green-600 hover:text-white text-xl cursor-pointer">
+                        Update password
+                    </button>
+                </div>
+            </form>
+        </div>
+    </section>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PlatController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [ HomeController::class, 'home'] )
+    ->name('home');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', [ HomeController::class, 'about'] )
+    ->name('about');
+
+Route::resource('plats', PlatController::class)
+    ->only(['index']);
 
 Route::middleware(['auth', 'isAdmin'])->group(function (){
     Route::resource('plats', PlatController::class)
@@ -23,6 +26,16 @@ Route::middleware(['auth'])->group(function (){
 
     Route::resource('plats', PlatController::class)
         ->only(['show']);
+
+    Route::get('profile', [UserController::class, 'index'])
+        ->name('profile.index');
+
+    Route::put('/profile/password', [UserController::class, 'updatePassword'])
+        ->name('profile.update.password');
+
+    Route::put('/profile', [UserController::class, 'updateProfile'])
+        ->name('profile.update');
+
 });
 
 Route::middleware(['guest'])->group(function (){
@@ -32,7 +45,18 @@ Route::middleware(['guest'])->group(function (){
     Route::match(['GET', 'POST'], '/register', [AuthController::class, 'register'])
         ->name('register');
 
+    Route::get('/forgot-password', function () {
+        return view('auth.forgot-password');
+    })->name('password.request');
+
+    Route::post('/forget-password', [AuthController::class, 'resetPassword'])
+        ->name('password.email');
+
+    Route::get('/reset-password/{token}', function ($token) {
+        return view('auth.reset-password', ['token' => $token]);
+    })->name('password.reset');
+
+    Route::post('/reset-password/{token}', [AuthController::class, 'updatePassword'])
+        ->name('password.update');
 });
 
-    Route::resource('plats', PlatController::class)
-        ->only(['index']);
